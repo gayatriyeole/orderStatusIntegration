9ba9a1df3357534ec540d0dce1c7a63dd90ce36f
GY <ruteshpatil@gy-macbook-pro.local>
2026-01-25 15:17:20 -0600

    Initial commit

---
+++ src/main/java/com/orderHub/orderStatusIntegration/OrderStatusIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.orderHub.orderStatusIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderStatusIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderStatusIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderHub/orderStatusIntegration/OrderStatusIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderHub.orderStatusIntegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderStatusIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
